e9bf572f90adffb6193783aac8d30b3c88f57b10
tjuchq <hebut18031002036@163.com>
2026-08-08 00:16:13 +0800

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -69,8 +69,8 @@ function createAssessmentModule() {
    *
    *   单调递减，相对排序与距离排序完全一致。
    * ========================================================== */
-  var SIMILARITY_PLATEAU = 10000;
-  var SIMILARITY_DECAY_BASE = 20000;
+  var SIMILARITY_PLATEAU = 20000;
+  var SIMILARITY_DECAY_BASE = 40000;
   var SIMILARITY_EXPONENT = 1.2;
 
   /**
